4afa97ab6bfa76afcc7af2eca7a0fea3c9b2ec09
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-19 09:56:25 -0300

    feat: add body parser json

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { Routes } from './routes';
+import { Routes } from '@routes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', Routes);
 
 app.get('/', (req, res) => {
