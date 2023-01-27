d2a657d8307ee63546ccbcef284e3286b5ab508a
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-27 12:22:51 -0300

    feat: exports all routes rooms

---
+++ src/controllers/rooms/routes/index.ts
@@ -0,0 +1,17 @@
+import { all } from './all';
+import { create } from './create';
+import { del } from './delete';
+import { unique } from './unique';
+import { update } from './update';
+import { connect } from './connect';
+import { leave } from './leave';
+
+export {
+	all,
+	create,
+	del,
+	unique,
+	update,
+	connect,
+	leave,
+};
