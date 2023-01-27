6a6987a2ce9175e8f85ddcca615d4aec5985a51e
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-27 12:26:20 -0300

    feat: configured all routes and validators to rooms

--- src/controllers/rooms/index.ts
+++ src/controllers/rooms/index.ts
@@ -1,21 +1,35 @@
+import { verifyIdUser } from '@validators/verifyIdUser';
 import { Router } from 'express';
-import { checkToken } from '../../middlewares/checkToken';
-import { all } from './all';
-import { create } from './create';
-import { del } from './delete';
-import { unique } from './unique';
-import { update } from './update';
+import { checkToken } from '@middlewares/checkToken';
+import {
+	all,
+	connect,
+	create,
+	del,
+	leave,
+	unique,
+	update
+} from './routes';
+import {
+	createValidator,
+	updateValidator,
+	uuidValidator
+} from './validators';
 
 const Rooms = Router();
 
-Rooms.get('/', checkToken, all);
+Rooms.get('/', checkToken, verifyIdUser, all);
 
-Rooms.get('/:id', checkToken, unique);
+Rooms.get('/:id', checkToken, uuidValidator, unique);
 
-Rooms.post('/', checkToken, create);
+Rooms.post('/', checkToken, verifyIdUser, createValidator, create);
 
-Rooms.patch('/:id', checkToken, update);
+Rooms.post('/connect/:id', checkToken, verifyIdUser, uuidValidator, connect);
 
-Rooms.delete('/:id', checkToken, del);
+Rooms.patch('/:id', checkToken, verifyIdUser, uuidValidator, updateValidator, update);
+
+Rooms.delete('/:id', checkToken, verifyIdUser, uuidValidator, del);
+
+Rooms.delete('/leave/:id', checkToken, verifyIdUser, uuidValidator, leave);
 
 export default Rooms;
