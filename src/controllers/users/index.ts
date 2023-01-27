a2c44843e3bf29866b5287d1da96fd137555e724
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-27 12:30:03 -0300

    feat: add validator verify id user in routes

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -2,15 +2,16 @@ import { Router } from 'express';
 import { checkToken } from '@middlewares/checkToken';
 import { create, del, getUnique, update } from './routes';
 import { createValidator, updateValidator } from './validators';
+import { verifyIdUser } from '@validators/verifyIdUser';
 
 const Users = Router();
 
 Users.post('/', createValidator, create);
 
-Users.get('/', checkToken, getUnique);
+Users.get('/', checkToken, verifyIdUser, getUnique);
 
-Users.patch('/', checkToken, updateValidator, update);
+Users.patch('/', checkToken, verifyIdUser, updateValidator, update);
 
-Users.delete('/', checkToken, del);
+Users.delete('/', checkToken, verifyIdUser, del);
 
 export default Users;
