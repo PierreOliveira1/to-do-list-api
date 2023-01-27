4534e99cce81fc45c20080c25169de7468c47774
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-27 12:16:45 -0300

    feat: configured socket initial

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { app } from './app';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-app.listen(3333, () => {
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+	socket.on('room', (message: string) => {
+		socket.join(message);
+	});
+});
+
+server.listen(3333, () => {
 	console.log('Running!!');
 });
