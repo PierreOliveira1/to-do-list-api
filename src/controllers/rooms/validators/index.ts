2cc985a1390a743999d99ea0f645f4088c6d9209
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-27 12:25:56 -0300

    feat: exports all validators

---
+++ src/controllers/rooms/validators/index.ts
@@ -0,0 +1,9 @@
+import { createValidator } from './createValidator';
+import { updateValidator } from './updateValidator';
+import { uuidValidator } from './uuidValidator';
+
+export {
+	createValidator,
+	updateValidator,
+	uuidValidator,
+};
